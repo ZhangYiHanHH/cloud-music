8d619fe8d647b780acbe4a2de9d10955a0aec5cf
ZHANGYIHANHH <1076335206@qq.com>
2019-01-19 17:58:43 +0800

    music

--- case-project/js/01.js
+++ case-project/js/01.js
@@ -4,8 +4,9 @@ let messages =[];
 $(function () {
 
     //请求   // 初始化加载
-    $.getJSON("data/message.json" , function (results) {
+    $.getJSON("../data/message.json" , function (results) {
         messages = results;
+        console.log(results)
     });
 
     //生成页面
@@ -14,15 +15,64 @@ $(function () {
         arr.forEach(function (e) {
 
             //替换成 自己的 HTML ⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐
-            let neirong = $("<div class=\"neirong-top \">\n" +
+            let neirong = $("<div class=\"dongtai \">\n" +
+                "                <div class=\"touxiang \">\n" +
+                "                    <div class=\"tou-pict  pict\" style=\"background-image: url("+e.photo+")\"></div>\n" +
+                "                </div>\n" +
+                "                <div class=\"neirong \">\n" +
+                "                    <div class=\"neirong-top \">\n" +
                 "                        <div class=\"nei-top-left \">\n" +
-                "                            <div class=\"left-top \">"+left-top+"</div>\n" +
-                "                            <div class=\"left-bottom \">"+left-top+"</div>\n" +
+                "                            <div class=\"left-top \">"+e.name+"</div>\n" +
+                "                            <div class=\"left-bottom \">"+e.amount+"</div>\n" +
                 "                        </div>\n" +
-                "                        <div class=\"nei-top-center \">"+nei-top-center+"</div>\n" +
-                "                        <div class=\"nei-top-right  xy-center\">"+nei-top-right+"</div>\n" +
+                "                        <div class=\"nei-top-center \">分享单曲：</div>\n" +
+                "                        <div class=\"nei-top-right  xy-center\">+关注</div>\n" +
                 "                    </div>\n" +
-                "                    <div class=\"words \">"+words+"</div>");
+                "                    <div class=\"words \">"+e.words+"" +
+                "                    </div>\n" +
+                "                    <div class=\"picture \">\n" +
+                "                        <div class=\" pict\" style=\"background-image: url("+e.picture1+")\"></div>\n" +
+                "                        <div class=\" pict\"style=\"background-image: url("+e.picture5+")\"></div>\n" +
+                "                        <div class=\" pict\" style=\"background-image: url("+e.picture2+")\"></div>\n" +
+                "                        <div class=\" pict\" style=\"background-image: url("+e.picture3+")\"></div>\n" +
+                "                        <div class=\" pict\" style=\"background-image: url("+e.picture4+")\"></div>\n" +
+                "                        <div class=\" pict\" style=\"background-image: url("+e.picture6+")\"></div>\n" +
+                "                        <div class=\" pict\" style=\"background-image: url("+e.picture7+")\"></div>\n" +
+                "                        <div class=\" pict\" style=\"background-image: url("+e.picture8+")\"></div>\n" +
+                "                        <div class=\" pict\"style=\"background-image: url("+e.picture9+")\"></div>\n" +
+                "                    </div>\n" +
+                "                    <div class=\"neirong-music \">\n" +
+                "                        <div class=\"music-pict  pict xy-center\" style=\"background-image: url(../img/friends/耳语.jpg)\">\n" +
+                "                            <div class=\"circle xy-center\" >\n" +
+                "                                <div class=\"pict tangle \" style=\"background-image: url(../img/friends/三角.svg)\"></div>\n" +
+                "                            </div>\n" +
+                "                        </div>\n" +
+                "                        <div class=\"music-font \">\n" +
+                "                            <div class=\"music-font1 \">耳语</div>\n" +
+                "                            <div class=\"music-font2 \">乖白</div>\n" +
+                "                        </div>\n" +
+                "\n" +
+                "                    </div>\n" +
+                "                    <div class=\"bottom \">\n" +
+                "                        <div class=\"bottom1  xy-center\">\n" +
+                "                            <div class=\"pict1  pict\" style=\"background-image: url(../img/friends/转发.svg)\"></div>\n" +
+                "                            <div class=\"font1 \">30</div>\n" +
+                "                        </div>\n" +
+                "                        <div class=\"bottom1  xy-center \" >\n" +
+                "                            <div class=\"pict1  pict\" style=\"background-image: url(../img/friends/评论.svg)\"></div>\n" +
+                "                            <div class=\"font1 \">40</div>\n" +
+                "                        </div>\n" +
+                "                        <div class=\"bottom1  xy-center \" >\n" +
+                "                            <div class=\"pict1  pict\" style=\"background-image: url(../img/friends/点赞.svg)\"></div>\n" +
+                "                            <div class=\"font1 \">50</div>\n" +
+                "                        </div>\n" +
+                "                        <div class=\"bottom1  xy-center \">\n" +
+                "                            <div class=\"pict1 pict\" style=\"background-image: url(../img/friends/三个点.svg)\"></div>\n" +
+                "                        </div>\n" +
+                "\n" +
+                "                    </div>\n" +
+                "                </div>\n" +
+                "            </div>");
             //⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐⭐
 
             $("#friends-dynastic").append(neirong);
@@ -31,7 +81,7 @@ $(function () {
     }
 
 //监听滚动
-    $("#container").scroll(function () {   //监听滚动事件..
+    $("#friends-dynastic").scroll(function () {   //监听滚动事件..
 
         let container_totalHeight = this.scrollHeight;  //总高度
 
